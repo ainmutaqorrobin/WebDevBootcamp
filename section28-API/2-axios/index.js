11b3294297c4b785263efeb8ef7a07478d151ec9
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-10-28 10:14:25 +0800

    axios api solution

--- section28-API/2-axios/index.js
+++ section28-API/2-axios/index.js
@@ -15,7 +15,9 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    console.log(result);
+
+    res.render("solution.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -26,6 +28,21 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   console.log(req.body);
+  const { type, participants } = req.body;
+  let url;
+  //https://bored-api.appbrewery.com/filter?type=education&participants=2
+
+  url = `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`;
+
+  try {
+    const response = await axios.get(url);
+    const result = response.data;
+    res.render("solution.ejs", { data: result[0] });
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+    res.render("solution.ejs", { error: error.message });
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
