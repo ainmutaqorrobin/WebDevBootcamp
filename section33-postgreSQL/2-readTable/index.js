7594bd12ca175b1ceeca2a4af3a5a5da7439a25c
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-10-31 08:07:16 +0800

    postgresql read data solution

--- section33-postgreSQL/2-readTable/index.js
+++ section33-postgreSQL/2-readTable/index.js
@@ -1,9 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123",
+  port: 5432,
+});
+
+db.connect();
+
+let quiz = [
+  { country: "France", capital: "Paris" },
+  { country: "United Kingdom", capital: "London" },
+  { country: "United States of America", capital: "New York" },
+];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) console.log("Error executing query", err.stack);
+  else quiz = res.rows;
+  db.end();
+});
 let totalCorrect = 0;
 
 // Middleware
