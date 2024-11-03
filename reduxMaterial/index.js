7d581be7d311040e8a2b7ad0537cc8498a7213d6
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-03 13:28:25 +0800

    multiple action in single reducer

--- reduxMaterial/index.js
+++ reduxMaterial/index.js
@@ -1,15 +1,23 @@
 import { legacy_createStore as createStore } from "redux";
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
     type: BUY_CAKE,
-    info: "First redux action",
+    info: "Customer purchased cake.",
+  };
+}
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "Customer purchased ice cream.",
   };
 }
 
 const shelf = {
   numOfCakes: 10,
+  numOfIceCreams: 20,
 };
 
 const reducer = (state = shelf, action) => {
@@ -19,7 +27,11 @@ const reducer = (state = shelf, action) => {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
-
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
     default:
       return state;
   }
@@ -34,4 +46,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
