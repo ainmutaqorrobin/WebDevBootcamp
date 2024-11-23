74eed2dcabc63de193bd3fd666e32c7d3ddba668
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-23 12:41:24 +0800

    add notification component

--- p-nextAPI/pages/_app.js
+++ p-nextAPI/pages/_app.js
@@ -1,17 +1,22 @@
-import Head from 'next/head';
-
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+import Notification from "../components/notification/notifcation";
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification
+        title="Test"
+        message="this is test notification"
+        status="success"
+      />
     </Layout>
   );
 }
