4fd4759f6a755d28c587110d5509abcec7757728
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-23 20:19:34 +0800

    using context data in component

--- p-nextAPI/components/layout/layout.js
+++ p-nextAPI/components/layout/layout.js
@@ -1,12 +1,23 @@
-import { Fragment } from 'react';
-
-import MainHeader from './main-header';
+import { Fragment, useContext } from "react";
+import Notification from "../components/notification/notifcation";
+import MainHeader from "./main-header";
+import { NotificationContext } from "../../store/notification-context";
 
 function Layout(props) {
+  const notifContext = useContext(NotificationContext);
+  const activeNotification = notifContext.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
--- p-nextAPI/pages/_app.js
+++ p-nextAPI/pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Notification from "../components/notification/notifcation";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import { NotificationContextProvider } from "../store/notification-context";
@@ -11,14 +10,12 @@ function MyApp({ Component, pageProps }) {
         <Head>
           <title>Next Events</title>
           <meta name="description" content="NextJS Events" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
         </Head>
         <Component {...pageProps} />
-        <Notification
-          title="Test"
-          message="this is test notification"
-          status="success"
-        />
       </Layout>
     </NotificationContextProvider>
   );
