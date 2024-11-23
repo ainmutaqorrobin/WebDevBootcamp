a4f3b27ae0eed8a257e2e9354b0a2620ce680124
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-23 20:12:33 +0800

    create notification context component

---
+++ p-nextAPI/store/notification-context.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+export const NotificationContext = createContext({
+  notification: null,
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
