577709c3a747d50331f6799fbf8f53e1ac962c58
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-11-24 12:43:16 +0800

    add loading state and notification status for adding comment

--- p-nextAPI/components/input/comments.js
+++ p-nextAPI/components/input/comments.js
@@ -1,34 +1,61 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
+import { NotificationContext } from "../../store/notification-context";
 
 function Comments(props) {
   const { eventId } = props;
-
+  const notifContext = useContext(NotificationContext);
   const [showComments, setShowComments] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
-  function addCommentHandler(commentData) {
-    fetch(`/api/comments/${eventId}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(commentData),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+  async function addCommentHandler(commentData) {
+    notifContext.showNotification({
+      title: "Processing",
+      message: "Adding new comments..",
+      status: "pending",
+    });
+    try {
+      const response = await fetch(`/api/comments/${eventId}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(commentData),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        return notifContext.showNotification({
+          title: "Success",
+          message: "New comment is added",
+          status: "success",
+        });
+      } else {
+        throw new Error(data.message || "Something went wrong!");
+      }
+    } catch (error) {
+      return notifContext.showNotification({
+        title: "Failed.",
+        message: error.message || "Fail to add new comment",
+        status: "error",
+      });
+    }
   }
 
   useEffect(() => {
     if (showComments) {
+      setLoading(true);
       fetch(`/api/comments/${eventId}`)
         .then((response) => response.json())
-        .then((data) => setComments(data.comments));
+        .then((data) => {
+          setComments(data.comments);
+          setLoading(false);
+        });
     }
   }, [showComments]);
   return (
@@ -37,7 +64,8 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !loading && <CommentList items={comments} />}
+      {showComments && loading && <p>Loading...</p>}
     </section>
   );
 }
