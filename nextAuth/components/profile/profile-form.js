76024dcf346cfd0ae55ec0b341a392de631088b5
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-12-19 01:01:30 +0800

    change password API Integration

--- nextAuth/components/profile/profile-form.js
+++ nextAuth/components/profile/profile-form.js
@@ -1,15 +1,53 @@
-import classes from './profile-form.module.css';
+import { useState } from "react";
+import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm({ submitForm }) {
+  const [formData, setFormData] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+
+  const resetForm = () => {
+    setFormData({
+      oldPassword: "",
+      newPassword: "",
+    });
+  };
+
+  const handleInput = (e) => {
+    const { id, value } = e.target;
+
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    submitForm(formData);
+    resetForm();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="newPassword"
+          onChange={handleInput}
+          value={formData.newPassword}
+          required
+        />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input
+          type="password"
+          id="oldPassword"
+          onChange={handleInput}
+          value={formData.oldPassword}
+          required
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- nextAuth/components/profile/user-profile.js
+++ nextAuth/components/profile/user-profile.js
@@ -2,10 +2,24 @@ import ProfileForm from "./profile-form";
 import styles from "./user-profile.module.css";
 
 function UserProfile() {
+  async function updatePassword(formData) {
+    try {
+      const response = await fetch("/api/profile/changepass", {
+        method: "PATCH",
+        body: JSON.stringify(formData),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <section className={styles.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm submitForm={updatePassword} />
     </section>
   );
 }
