27cc8ef9fe62ed29ec11d10948fadba35f6105a6
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-12-21 21:44:54 +0800

    solution 1

--- maincourse/section33-postgreSQL/4-uniquenotnull/index.js
+++ maincourse/section33-postgreSQL/4-uniquenotnull/index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  password: "123",
+  host: "localhost",
+  port: 5432,
+  database: "world",
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  let countries = [];
+
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  result.rows.forEach((country) => countries.push(country.country_code));
+
+  res.render("index.ejs", {
+    countries,
+    total: countries.length,
+  });
+
+  db.end();
 });
 
 app.listen(port, () => {
