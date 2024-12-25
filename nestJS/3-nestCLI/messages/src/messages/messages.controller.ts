9f15180dee36e8af2f1e9f502c159ad02648e2a8
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-12-25 22:31:37 +0800

    added messages service to controller

--- nestJS/3-nestCLI/messages/src/messages/messages.controller.ts
+++ nestJS/3-nestCLI/messages/src/messages/messages.controller.ts
@@ -1,19 +1,25 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
   getAllMessages() {
-    return [];
+    return this.messagesService.findAll();
   }
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(typeof id);
+    return this.messagesService.findOne(id);
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDTO) {
-    console.log(body);
+    return this.messagesService.createOne(body.message);
   }
 }
