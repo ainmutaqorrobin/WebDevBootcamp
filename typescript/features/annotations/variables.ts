d5c0267f17c882b1353dc5b59bf41193dbc4badb
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-26 16:04:10 +0800

    typescript learning material #2

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,18 @@
+let apples: number = 5;
+let speed: string = "fast";
+let isName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ["red", "green"];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
