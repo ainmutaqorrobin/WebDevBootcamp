6af49adee7f0bda0e5d88d1e69c947f22b05636c
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-29 19:34:45 +0800

    typescript learning material #6

--- typescript/features/annotations/arrays.ts
+++ typescript/features/annotations/arrays.ts
@@ -6,8 +6,14 @@ const carsByMake = [["f150", "corolla", "camaro"]];
 const cars = carMakers[0];
 const myCar = carMakers.pop();
 
-carMakers.push(100);
+// carMakers.push(100);
 
 carMakers.map((car: string) => {
   return car.toUpperCase();
 });
+
+//Flexible types
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
