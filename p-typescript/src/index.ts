6a855983944594b7b317667f36ab2f192986a230
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-30 21:52:10 +0800

    add google map api

--- p-typescript/src/index.ts
+++ p-typescript/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hi there");
+/// <reference types="@types/google.maps" />
+
+import { Company } from "./Company";
+import { User } from "./User";
+
+const user = new User();
+const company = new Company();
+console.log(user);
+console.log(company);
+
