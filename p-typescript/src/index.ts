61691769f1d3e641bab01cbff903ecaae93fb912
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-01-31 22:23:06 +0800

    add map display

--- p-typescript/src/index.ts
+++ p-typescript/src/index.ts
@@ -1,10 +1,6 @@
 /// <reference types="@types/google.maps" />
 
-import { Company } from "./Company";
-import { User } from "./User";
-
-const user = new User();
-const company = new Company();
-console.log(user);
-console.log(company);
-
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: { lat: 0, lng: 0 },
+});
