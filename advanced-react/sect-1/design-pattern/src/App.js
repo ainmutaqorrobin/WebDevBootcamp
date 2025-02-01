0d7f94b38882b4b8131fc0b155ccf30cfa166eec
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-02 01:08:04 +0800

    list initial

--- advanced-react/sect-1/design-pattern/src/App.js
+++ advanced-react/sect-1/design-pattern/src/App.js
@@ -1,19 +1,7 @@
-import "./App.css";
-import { SplitScren } from "./components/split-screen";
+import React from "react";
 
-const LeftSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
-};
-const RightSideComponent = ({ title }) => {
-  return <h2 style={{ backgroundColor: "yellow" }}>{title}</h2>;
-};
 function App() {
-  return (
-    <SplitScren Left={LeftSideComponent} Right={RightSideComponent}>
-      <LeftSideComponent title={"Right"} />
-      <RightSideComponent title={"Left"} />
-    </SplitScren>
-  );
+  return <div>App</div>;
 }
 
 export default App;
