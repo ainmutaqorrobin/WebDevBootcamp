28172510418d984078b8603567af341440becc55
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-02 00:50:19 +0800

    using styled-components

--- advanced-react/sect-1/design-pattern/src/App.js
+++ advanced-react/sect-1/design-pattern/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SplitScren } from "./components/split-screen";
 
+const LeftSideComponent = () => {
+  return <h2 style={{ backgroundColor: "crimson" }}>AT LEFT</h2>;
+};
+const RightSideComponent = () => {
+  return <h2 style={{ backgroundColor: "yellow" }}>AT RIGHT</h2>;
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SplitScren Left={LeftSideComponent} Right={RightSideComponent} />;
 }
 
 export default App;
