46171436f32315787681203e180b1ec34d3a4614
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-01 17:05:14 +0800

    i dont know what i know

--- p-typescript/src/CustomMap.ts
+++ p-typescript/src/CustomMap.ts
@@ -1,3 +1,8 @@
+/// <reference types="@types/google.maps" />
+
+import { Company } from "./Company";
+import { User } from "./User";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
   constructor(htmlId: string) {
@@ -9,4 +14,19 @@ export class CustomMap {
       }
     );
   }
+
+  async addUserMarker(user: User): Promise<void> {
+    const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+      "marker"
+    )) as google.maps.MarkerLibrary;
+
+    new AdvancedMarkerElement({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+  addCompanyMarker(company: Company): void {}
 }
--- p-typescript/src/index.ts
+++ p-typescript/src/index.ts
@@ -1,5 +1,14 @@
 /// <reference types="@types/google.maps" />
 
 import { CustomMap } from "./CustomMap";
+import { User } from "./User";
 
-new CustomMap("map");
+function initMap() {
+  const customMap = new CustomMap("map");
+  const user = new User();
+
+  customMap.addUserMarker(user);
+  console.log("test");
+}
+
+(window as any).initMap = initMap;
