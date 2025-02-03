7debb332d1922ad65f9986f0041bede465540299
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-03 23:36:14 +0800

    local storage data loader

--- advanced-react/sect-2/container-component/src/App.js
+++ advanced-react/sect-2/container-component/src/App.js
@@ -1,18 +1,31 @@
 import axios from "axios";
 import UserInfo from "./components/user-info";
 import DataSourceRender from "./components/data-source-render";
+import DataSource from "./components/data-source";
 
 function App() {
-  async function getUser(url) {
+  async function getData(url) {
     const response = await axios.get(url);
     return response.data;
   }
+
+  function getDataFromLocal(key) {
+    return localStorage.getItem(key);
+  }
+
+  const Message = ({ msg }) => {
+    return <h1>{msg}</h1>;
+  };
   return (
     <>
       <DataSourceRender
-        getData={() => getUser("/users/1")}
+        getData={() => getData("/users/1")}
         render={(resource) => <UserInfo user={resource} />}
       ></DataSourceRender>
+
+      <DataSource getData={() => getDataFromLocal("test")} resourceName={"msg"}>
+        <Message />
+      </DataSource>
     </>
   );
 }
