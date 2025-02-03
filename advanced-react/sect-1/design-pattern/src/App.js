fee5849e5588bb3a119513613d98598f29de8a2b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-03 20:00:24 +0800

    modal completed

--- advanced-react/sect-1/design-pattern/src/App.js
+++ advanced-react/sect-1/design-pattern/src/App.js
@@ -1,35 +1,19 @@
 import React from "react";
-import RegularList from "./components/lists/RegularList";
-import { authors } from "./data/authors";
-import { books } from "./data/books";
-import DetailAuthor from "./components/authors/DetailAuthor";
-import FullDetailAuthor from "./components/authors/FullDetailAuthor";
-import DetailBook from "./components/books/DetailBook";
+import Modal from "./components/Modal";
 import FullDetailBook from "./components/books/FullDetailBook";
+import { books } from "./data/books";
 import NumberedList from "./components/lists/Numbered";
 
 function App() {
   return (
     <>
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        Component={DetailAuthor}
-      />
-      <NumberedList
-        items={authors}
-        sourceName={"author"}
-        Component={FullDetailAuthor}
-      />
-      <p>
-        ========================================================================================
-      </p>
-      <RegularList items={books} sourceName={"book"} Component={DetailBook} />
-      <NumberedList
-        items={books}
-        sourceName={"book"}
-        Component={FullDetailBook}
-      />
+      <Modal>
+        <NumberedList
+          Component={FullDetailBook}
+          items={books}
+          sourceName="book"
+        />
+      </Modal>
     </>
   );
 }
