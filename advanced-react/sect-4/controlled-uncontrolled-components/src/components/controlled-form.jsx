a0e60c2f9e32958a6245ff3f6e8d3d430fb96299
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-02-04 08:04:21 +0800

    controlled components

---
+++ advanced-react/sect-4/controlled-uncontrolled-components/src/components/controlled-form.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function ControlledForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+  });
+
+  function resetForm() {
+    setFormData({ name: "", age: "" });
+  }
+  function handleInput(e) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.clear();
+    console.log(formData);
+    console.log(!!formData.name);
+
+    resetForm();
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        onChange={handleInput}
+        value={formData.name}
+      />
+      <input
+        type="number"
+        name="age"
+        placeholder="Age"
+        onChange={handleInput}
+        value={formData.age}
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default ControlledForm;
--- advanced-react/sect-4/controlled-uncontrolled-components/src/components/uncontrolled-form.jsx
+++ advanced-react/sect-4/controlled-uncontrolled-components/src/components/uncontrolled-form.jsx
@@ -1,47 +1,48 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function UncontrolledForm() {
-  const [formData, setFormData] = useState({
-    name: "",
-    age: "",
-  });
+// // WRONG IMPLEMENTATION, UNCONTROLLED FORM IS USING USEREF NOT USESTATE
+// function UncontrolledForm() {
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     age: "",
+//   });
 
-  function resetForm() {
-    setFormData({ name: "", age: "" });
-  }
-  function handleInput(e) {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  }
+//   function resetForm() {
+//     setFormData({ name: "", age: "" });
+//   }
+//   function handleInput(e) {
+//     const { name, value } = e.target;
+//     setFormData((prevData) => ({ ...prevData, [name]: value }));
+//   }
 
-  function submitHandler(e) {
-    e.preventDefault();
-    console.clear();
-    console.log(formData);
-    console.log(!!formData.name);
+//   function submitHandler(e) {
+//     e.preventDefault();
+//     console.clear();
+//     console.log(formData);
+//     console.log(!!formData.name);
 
-    resetForm();
-  }
+//     resetForm();
+//   }
 
-  return (
-    <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        onChange={handleInput}
-        value={formData.name}
-      />
-      <input
-        type="number"
-        name="age"
-        placeholder="Age"
-        onChange={handleInput}
-        value={formData.age}
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={submitHandler}>
+//       <input
+//         type="text"
+//         name="name"
+//         placeholder="Name"
+//         onChange={handleInput}
+//         value={formData.name}
+//       />
+//       <input
+//         type="number"
+//         name="age"
+//         placeholder="Age"
+//         onChange={handleInput}
+//         value={formData.age}
+//       />
+//       <input type="submit" value="Submit" />
+//     </form>
+//   );
+// }
 
-export default UncontrolledForm;
+// export default UncontrolledForm;
