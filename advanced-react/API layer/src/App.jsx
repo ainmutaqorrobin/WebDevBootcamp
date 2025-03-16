692de1d4ad877dba24dfd2424c80c0b02a05ca59
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-03-17 01:01:46 +0800

    practising request abort logic

--- advanced-react/API layer/src/App.jsx
+++ advanced-react/API layer/src/App.jsx
@@ -1,9 +1,10 @@
+import SearchMeal from "./components/search-meals";
 import Users from "./components/users";
 
 function App() {
   return (
     <>
-      <Users />
+      <SearchMeal />
     </>
   );
 }
