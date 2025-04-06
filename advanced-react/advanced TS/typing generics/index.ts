bd68fa66613dced98a1c2163947e77d533935e8c
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-06 08:03:35 +0800

    Typescript utility

--- advanced-react/advanced TS/typing generics/index.ts
+++ advanced-react/advanced TS/typing generics/index.ts
@@ -12,3 +12,33 @@ type set2 = "b" | "c" | "d";
 
 type union = set1 | set2;
 type intersection = set1 & set2;
+
+type Obj2 = { a: string };
+
+type ObjWithStringKeys = { [key: string]: number };
+
+type ObjWithSpecificKeys = { [K in "a" | "b" | "c"]: number };
+// type ObjWithSpecificKeys = {
+//   a: number;
+//   b: number;
+//   c: number;
+// };
+
+type mask = {
+  [K in keyof ObjWithSpecificKeys]: boolean;
+};
+const sample: Partial<ObjWithSpecificKeys> = {
+  a: 2,
+};
+
+type newType = Pick<Obj, "a" | "c">; //if you want to create new type based on existed type
+// type newType = {
+//   a: "A";
+//   c: number;
+// };
+
+type excludeType = Omit<Obj, "b">; //if you want to create new type excluding some of key existed type
+// type excludeType = {
+//   a: "A";
+//   c: number;
+// };
