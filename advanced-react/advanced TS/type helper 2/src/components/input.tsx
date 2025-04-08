e8ffed9bf854d01d51522da9323908d7e745a2c2
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-09 03:17:12 +0800

    type helper 2 using generic

--- advanced-react/advanced TS/type helper 2/src/components/input.tsx
+++ advanced-react/advanced TS/type helper 2/src/components/input.tsx
@@ -1,17 +1,13 @@
 import { ChangeEventHandler } from "react";
 
-type InputProps = (
-  | {
-      value: string;
-      onChange: ChangeEventHandler;
-    }
-  | {
-      value?: undefined;
-      onChange?: undefined;
-    }
-) & {
-  label: string;
-};
+type RequiredProps<T> = T | OptionalProps<T>;
+
+type OptionalProps<T> = Partial<Record<keyof T, undefined>>;
+
+type InputProps = RequiredProps<{
+  value: string;
+  onChange: ChangeEventHandler;
+}> & { label: string };
 
 const Input = ({ label, ...props }: InputProps) => {
   return (
