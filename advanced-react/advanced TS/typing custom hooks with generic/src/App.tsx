c2196bd6d4919d4fea4c784de75d01a4edc84824
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-10 20:35:42 +0800

    typing hooks with generic

--- advanced-react/advanced TS/typing custom hooks with generic/src/App.tsx
+++ advanced-react/advanced TS/typing custom hooks with generic/src/App.tsx
@@ -1,9 +1,9 @@
-const useLocalStorage = (identifier: string) => {
-  const set = (key: string, value: any) => {
+const useLocalStorage = <T,>(identifier: string) => {
+  const set = (key: string, value: T) => {
     window.localStorage.setItem(key + identifier, JSON.stringify(value));
   };
 
-  const get = (key: string) => {
+  const get = (key: string): T | null => {
     return JSON.parse(window.localStorage.getItem(key + identifier) || "null");
   };
   return { set, get };
