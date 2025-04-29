8b2520891293ee3a4a9b995e94d82d2fbb15ea03
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-29 22:40:30 +0800

    initial material

---
+++ advanced-react/design system - patterns for spacing/src/components/4-grid-pattern/start.jsx
@@ -0,0 +1,17 @@
+import Card from "./card";
+
+const Cards = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Cards;
