74c850fd9b737019a4f372e283f2907968e3628b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-29 15:49:26 +0800

    finalize column pattern

--- advanced-react/design system - patterns for spacing/src/components/3-column-pattern/start.jsx
+++ advanced-react/design system - patterns for spacing/src/components/3-column-pattern/start.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
 import { Split } from "../2-split-pattern/final";
 import { InputGroup } from "../2-split-pattern/form";
+import { spaceSchema } from "../common/spaces";
+
+const Columns = styled.div`
+  --columns: ${({ columns = 1 }) => columns};
+  display: grid;
+  gap: ${({ gutter }) => spaceSchema[gutter] ?? spaceSchema.l};
+  grid-template-columns: repeat(var(--columns), 1fr);
+`;
+
+const Column = styled.div`
+  grid-column: span ${({ $span = 1 }) => $span};
+`;
 
 const InfoFormWithColumns = () => {
   return (
@@ -18,29 +31,49 @@ const InfoFormWithColumns = () => {
 
 const Form = () => {
   return (
-    <div>
-      <InputGroup htmlFor="firstName" label="First Name">
-        <input type="text" id="firstName" />
-      </InputGroup>
-      <InputGroup htmlFor="lastName" label="Last Name">
-        <input type="text" id="lastName" />
-      </InputGroup>
-      <InputGroup htmlFor="email" label="Email">
-        <input type="text" id="email" />
-      </InputGroup>
-      <InputGroup htmlFor="address" label="Street Address">
-        <input type="text" id="address" />
-      </InputGroup>
-      <InputGroup htmlFor="city" label="City">
-        <input type="text" id="city" />
-      </InputGroup>
-      <InputGroup htmlFor="country" label="Country">
-        <input type="text" id="country" />
-      </InputGroup>
-      <InputGroup htmlFor="phone" label="Phone Number">
-        <input type="text" id="phone" />
-      </InputGroup>
-    </div>
+    <Columns columns={4}>
+      <Column $span={3}>
+        <InputGroup htmlFor="firstName" label="First Name">
+          <input type="text" id="firstName" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={3}>
+        <InputGroup htmlFor="lastName" label="Last Name">
+          <input type="text" id="lastName" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={4}>
+        <InputGroup htmlFor="email" label="Email">
+          <input type="text" id="email" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={6}>
+        <InputGroup htmlFor="address" label="Street Address">
+          <input type="text" id="address" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={2}>
+        <InputGroup htmlFor="city" label="City">
+          <input type="text" id="city" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={2}>
+        <InputGroup htmlFor="country" label="Country">
+          <input type="text" id="country" />
+        </InputGroup>
+      </Column>
+
+      <Column $span={2}>
+        <InputGroup htmlFor="phone" label="Phone Number">
+          <input type="text" id="phone" />
+        </InputGroup>
+      </Column>
+    </Columns>
   );
 };
 
