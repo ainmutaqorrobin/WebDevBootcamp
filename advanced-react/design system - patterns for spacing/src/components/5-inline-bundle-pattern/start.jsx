788f9591f57692bb8a11fad82d23a3facc07dec4
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-30 06:28:38 +0800

    finalize inline bundle pattern

--- advanced-react/design system - patterns for spacing/src/components/5-inline-bundle-pattern/start.jsx
+++ advanced-react/design system - patterns for spacing/src/components/5-inline-bundle-pattern/start.jsx
@@ -1,15 +1,35 @@
+import styled from "styled-components";
 import MenuBasis from "./menu";
+import { spaceSchema } from "../common/spaces";
+
+const justifySchema = {
+  start: "flex-start",
+  end: "flex-end",
+  center: "center",
+};
+
+const InlineBundle = styled.div`
+  --gutter: ${({ gutter }) => spaceSchema[gutter] ?? spaceSchema.l};
+  --justify: ${({ justify }) => justifySchema[justify] ?? justifySchema.start};
+  --align: ${({ align }) => justifySchema[align] ?? justifySchema.start};
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--gutter);
+
+  justify-content: var(--justify);
+  align-items: var(--align);
+`;
 
 const Menu = () => {
   return (
     <MenuBasis>
-      <div>
+      <InlineBundle justify="end" gutter="xxl" align="top">
         <span>Books</span>
         <span>Authors</span>
         <span>Deals</span>
         <span>About Us</span>
         <span>Sign-in</span>
-      </div>
+      </InlineBundle>
     </MenuBasis>
   );
 };
