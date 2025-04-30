6d6e10ec0460f6a6b88c83af7ed5cb147fd1161b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-04-30 22:14:41 +0800

    finalize inline pattern

--- advanced-react/design system - patterns for spacing/src/components/6-inline-pattern/start.jsx
+++ advanced-react/design system - patterns for spacing/src/components/6-inline-pattern/start.jsx
@@ -1,10 +1,42 @@
+import styled, { css } from "styled-components";
 import { InlineBundle } from "../5-inline-bundle-pattern/start";
 import { Button, Logo, MenuWrapper } from "./components";
+
+const stretchSchema = {
+  all: `>* {flex:1}`,
+  start: `> :first-child {flex:1}`,
+  end: `>:last-child {flex:1}`,
+};
+
+const responsive = css`
+  --switchAt: ${({ switchAt }) =>
+    typeof switchAt === "string" ? switchAt : `${switchAt}px`};
+
+  flex-wrap: wrap;
+
+  & > * {
+    min-width: fit-content;
+    flex-basis: calc((var(--switchAt) - (100% - var(--gutter))) * 999);
+  }
+`;
+
+export const Inline = styled(InlineBundle)`
+  flex-wrap: nowrap;
+  ${({ stretch }) => {
+    if (typeof stretch === "number") {
+      return `>:nth-child(${stretch + 1}) {flex:1}`;
+    }
+
+    return stretchSchema[stretch] ?? "";
+  }}
+
+  ${({ switchAt }) => switchAt && responsive}
+`;
 
 const Menu = () => {
   return (
     <MenuWrapper>
-      <div>
+      <Inline stretch={1} switchAt="40rem">
         <div>
           <Logo />
         </div>
@@ -14,11 +46,11 @@ const Menu = () => {
           <span>Deals</span>
           <span>About Us</span>
         </InlineBundle>
-        <div>
+        <Inline justify="end" align="center">
           <span>Login</span>
           <Button>Register</Button>
-        </div>
-      </div>
+        </Inline>
+      </Inline>
     </MenuWrapper>
   );
 };
