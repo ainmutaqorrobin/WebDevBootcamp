e89fa10b5da1c0f59e51cb640306e3a6c87c7a08
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-01 22:05:18 +0800

    finalize Pad pattern

--- advanced-react/design system - patterns for spacing/src/components/7-pad-pattern/start.jsx
+++ advanced-react/design system - patterns for spacing/src/components/7-pad-pattern/start.jsx
@@ -1,39 +1,56 @@
+import styled from "styled-components";
 import { Layers } from "../1-layers-pattern/start";
 import { Grid } from "../4-grid-pattern/start";
 import { InlineBundle } from "../5-inline-bundle-pattern/start";
-import {
-  Bottom,
-  Button,
-  Card,
-  Description,
-  Name,
-  Price,
-  Top,
-} from "./component";
+import { Bottom, Card, Description, Name, Price, Top } from "./component";
+import { spaceSchema } from "../common/spaces";
+
+export const Pad = styled.div`
+  padding: ${({ padding }) => {
+    return []
+      .concat(padding)
+      .map((paddingKey) => spaceSchema[paddingKey])
+      .join(" ");
+  }};
+`;
+
+export const Button = styled(Pad).attrs(() => ({
+  as: "button",
+  padding: ["s", "l"],
+}))`
+  background-color: crimson;
+  color: white;
+  border: none;
+  border-radius: 4px;
+`;
 
 const GiftCard = () => {
   return (
     <Card>
-      <Top>
-        <Name>Gift Card</Name>
-        <Description>This is one of our gift cards you can buy.</Description>
-        <Price>$25.99</Price>
-        <InlineBundle gutter="none" justify="center">
-          <Button>Buy</Button>
-        </InlineBundle>
-      </Top>
+      <Pad padding="l">
+        <Top>
+          <Name>Gift Card</Name>
+          <Description>This is one of our gift cards you can buy.</Description>
+          <Price>$25.99</Price>
+          <InlineBundle gutter="none" justify="center">
+            <Button>Buy</Button>
+          </InlineBundle>
+        </Top>
+      </Pad>
 
       <Bottom>
-        <Layers gutter="m">
-          <span>Includes:</span>
-          <ul>
-            <li>This is inclusion number 1</li>
-            <li>
-              This is inclusion number 2 which comes after inclusion number1
-            </li>
-            <li>This is inclusion number 3</li>
-          </ul>
-        </Layers>
+        <Pad padding="l">
+          <Layers gutter="m">
+            <span>Includes:</span>
+            <ul>
+              <li>This is inclusion number 1</li>
+              <li>
+                This is inclusion number 2 which comes after inclusion number1
+              </li>
+              <li>This is inclusion number 3</li>
+            </ul>
+          </Layers>
+        </Pad>
       </Bottom>
     </Card>
   );
