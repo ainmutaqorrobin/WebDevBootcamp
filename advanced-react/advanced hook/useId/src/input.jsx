c279da77ff079230eaba0060906ee199351958c5
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-07 14:43:00 +0800

    useId for unique id

--- advanced-react/advanced hook/useId/src/input.jsx
+++ advanced-react/advanced hook/useId/src/input.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 
 const Form = () => {
   const [email, setEmail] = useState("");
+  const id = useId();
   return (
     <div>
-      <label htmlFor="email">Email</label>
+      <label htmlFor={`${id}-email`}>Email</label>
       <input
-        id="email"
+        id={`${id}-email`}
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
+      <label htmlFor={`${id}-name`}>Name</label>
+      <input id={`${id}-name`} type="text" />
     </div>
   );
 };
