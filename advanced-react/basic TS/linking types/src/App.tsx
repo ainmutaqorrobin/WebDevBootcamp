9fc2697d1e80a03a60336e3b51c9a0f417388536
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-13 20:12:43 +0800

    initial material

---
+++ advanced-react/basic TS/linking types/src/App.tsx
@@ -0,0 +1,15 @@
+import Button from "./components/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="tertiary"></Button>
+
+      <Button variant="danger"></Button>
+    </>
+  );
+}
+
+export default App;
