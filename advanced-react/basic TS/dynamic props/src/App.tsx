6ce156185746504c9c7898d85237ce8b6efe6d01
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-14 04:53:27 +0800

    initial material

---
+++ advanced-react/basic TS/dynamic props/src/App.tsx
@@ -0,0 +1,15 @@
+import Button from "./components/buttons";
+
+function App() {
+  return (
+    <>
+      <Button variant="reset"></Button>
+      <Button variant="submit"></Button>
+      <Button variant="skip"></Button>
+
+      <Button variant="blabla"></Button>
+    </>
+  );
+}
+
+export default App;
