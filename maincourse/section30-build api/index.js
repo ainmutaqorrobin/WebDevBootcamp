b044dc2277c9a3cef17ec5c9455e307b84f3b869
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-05-30 11:04:28 +0800

    refresh express knowledge #3

--- maincourse/section30-build api/index.js
+++ maincourse/section30-build api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { Joi, validate, ValidationError } from "express-validation";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,20 @@ function getRandomNumber(max) {
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const updateJokeValidation = {
+  body: Joi.object({
+    jokeId: Joi.number().required().messages({
+      "any.required": "jokeId is required",
+    }),
+    jokeType: Joi.string().required().messages({
+      "any.required": "jokeType is required",
+    }),
+    jokeText: Joi.string().required().messages({
+      "any.required": "jokeText is required",
+    }),
+  }).options({ abortEarly: false }),
+};
 
 //1. GET a random joke
 app.get("/random", (req, res) => {
@@ -27,7 +42,7 @@ app.get("/joke/:id", (req, res) => {
   const jokeId = +id;
 
   if (!jokeId) {
-    return res.json({ message: "Please provide id", status: 404 });
+    return res.status(404).json({ message: "Please provide id", status: 404 });
   }
 
   const getJoke = jokes.find((joke) => joke.id === jokeId);
@@ -37,30 +52,36 @@ app.get("/joke/:id", (req, res) => {
       status: 404,
     });
   }
-  return res.json({ message: "success", status: 200, joke: getJoke });
+  return res
+    .status(200)
+    .json({ message: "success", status: 200, joke: getJoke });
 });
 
 //3. GET a jokes by filtering on the joke type
 app.get("/jokeType/:type", (req, res) => {
   const { type } = req.params;
   if (!type) {
-    return res.json({ status: 404, message: "Please provide joke type" });
+    return res
+      .status(404)
+      .json({ status: 404, message: "Please provide joke type" });
   }
 
   const selectedJokes = jokes.filter((joke) => joke.jokeType === type);
   if (selectedJokes.length === 0) {
-    return res.json({
+    return res.status(400).json({
       status: 400,
       message: "There are no jokes based on provide type",
     });
   }
-  return res.json({ status: 200, message: "success", jokes: selectedJokes });
+  return res
+    .status(200)
+    .json({ status: 200, message: "success", jokes: selectedJokes });
 });
 //4. POST a new joke
 app.post("/joke", (req, res) => {
   const { jokeText, jokeType } = req.body;
   if (!jokeText || !jokeType) {
-    return res.json({
+    return res.status(400).json({
       status: 400,
       message: "Please provide both joke text and joke type",
     });
@@ -73,15 +94,55 @@ app.post("/joke", (req, res) => {
   };
 
   jokes.push(newJoke);
-  return res.json({ status: 201, message: "new joke added", jokes });
+  return res
+    .status(201)
+    .json({ status: 201, message: "new joke added", jokes });
 });
+
 //5. PUT a joke
+app.put("/updateJoke", validate(updateJokeValidation), (req, res) => {
+  const { jokeId, jokeText, jokeType } = req.body;
+  const index = jokes.findIndex((joke) => joke.id === +jokeId);
+
+  if (index === -1) {
+    return res.status(400).json({
+      status: 400,
+      message: "There is no joke based on provided Id",
+    });
+  }
+
+  jokes[index] = {
+    id: +jokeId,
+    jokeText,
+    jokeType,
+  };
+
+  return res.status(200).json({ message: "Joke updated", jokes });
+});
 
 //6. PATCH a joke
 
 //7. DELETE Specific joke
 
 //8. DELETE All jokes
+
+//error message middleware
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    // Correctly extract messages from details.body (array of error objects)
+    const messages = (err.details?.body || []).map((detail) => detail.message);
+
+    return res.status(err.statusCode).json({
+      status: err.statusCode,
+      errors: messages,
+    });
+  }
+
+  return res.status(500).json({
+    status: 500,
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
