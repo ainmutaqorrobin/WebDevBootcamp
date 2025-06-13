ffd562eafb4e721b48a4c8c26085c9bb80135be5
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-06-13 15:18:54 +0800

    remove async from util

--- maincourse/section35-auth/util/encryption.js
+++ maincourse/section35-auth/util/encryption.js
@@ -1,9 +1,9 @@
 import bcrypt from "bcrypt";
 const saltRounds = 10;
-export const HashPassword = async (password) => {
-  return await bcrypt.hash(password, saltRounds);
+export const HashPassword = (password) => {
+  return bcrypt.hash(password, saltRounds);
 };
 
-export const VerifyPassword = async (enteredPassword, userPassword) => {
+export const VerifyPassword = (enteredPassword, userPassword) => {
   return bcrypt.compare(enteredPassword, userPassword);
 };
